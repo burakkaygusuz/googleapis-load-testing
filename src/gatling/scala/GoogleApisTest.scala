34bb686de8c931b563a8ae0b019d0da79ba2c0da
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-04-29 10:04:28 +0300

    The first scenario refactored

--- src/gatling/scala/GoogleApisTest.scala
+++ src/gatling/scala/GoogleApisTest.scala
@@ -1,16 +1,29 @@
+import java.util.Properties
+
 import io.gatling.core.Predef._
+import io.gatling.core.structure._
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
 
-class GoogleApisTest extends Simulation{
+class GoogleApisTest extends Simulation {
 
-  val httpConfig = http
+  val httpConfig: HttpProtocolBuilder = http
     .baseUrl("https://www.googleapis.com/blogger/v3/")
     .header(HttpHeaderNames.Accept, HttpHeaderValues.ApplicationJson)
 
-  val scn = scenario("Retrieving A Blog")
-    .exec(http("Get Blog By Id")
-      .get("blogs/2399953")
-      .queryParam("key", "AIzaSyBENR72f99cvvKiVGLHDjYD7kQk64-FJNk").check(status.is(200)))
+  val props = new Properties()
+  props.load(this.getClass.getResourceAsStream("gradle.properties"))
+
+  val API_KEY: String = props.getProperty("API_KEY")
+
+  val scn: ScenarioBuilder = scenario("Retrieving A Blog")
+    .exec(getBlogById(API_KEY, "2399953"))
+
+  def getBlogById(apiKey: String, blogId: String): ChainBuilder = {
+    exec(http("Get Blog By Id")
+      .get(s"blogs/$blogId")
+      .queryParam("key", apiKey).check(status.is(200)))
+  }
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpConfig))
 
